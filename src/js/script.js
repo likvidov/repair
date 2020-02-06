df96f6ea5db2d849a42da3fd501efb6dc0f8ca70
likvidov <likvidya@gmail.com>
2020-02-06 04:46:35 +0300

    Сверстан блок price

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ let button = document.querySelector('#button')
 let close = document.querySelector('#close')
 let modalWindow = document.querySelector('#modal')
 
+new WOW().init();
 
 button.addEventListener('click', () => {
     modal.classList.add('modal_active');
