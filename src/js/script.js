bd629b98813e719953edb1daa798b20ea13515b2
likvidov <likvidya@gmail.com>
2020-02-06 03:20:00 +0300

    Сверстан блок портфолио и настроен слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+
 let button = document.querySelector('#button')
 let close = document.querySelector('#close')
 let modalWindow = document.querySelector('#modal')
@@ -9,4 +10,38 @@ button.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
     modal.classList.remove('modal_active');
+});
+
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
 });
